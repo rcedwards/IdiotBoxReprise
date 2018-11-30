8167ec0aab856301582c8cd34f73a23a4171ee42
Robert Edwards <rcedwards@gmail.com>
2018-11-30 16:10:55 -0500

    Add tests for episode fetching on other pages

--- TVDBKit/TVDBKit/Episodes/EpisodeTests.swift
+++ TVDBKit/TVDBKit/Episodes/EpisodeTests.swift
@@ -31,4 +31,43 @@ class EpisodeTests: XCTestCase {
         }
         waitForExpectations(timeout: 5, handler: nil)
     }
+
+    func testFetchingSubsequentPage() throws {
+        let showIdentifier = 73739
+        let page = 2
+        let callbackExpectation = expectation(description: "Async")
+        authService.aquireToken(withLogin: login).then {
+            let authenticatedClient = AuthenticatedHTTPClient(baseAddress: self.baseAddress, token: $0)
+            self.episodesService = EpisodeAPIService(authenticatedHTTPClient: authenticatedClient)
+            self.episodesService.fetchEpisodes(forShow: showIdentifier, page: page).then {
+                XCTAssert($0.episodes.count > 0)
+                callbackExpectation.fulfill()
+                }.catch {
+                    XCTFail("\($0)")
+            }
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+
+    func testFetchingMissingPage() throws {
+        let showIdentifier = 73739
+        let page = 20
+        let callbackExpectation = expectation(description: "Async")
+        authService.aquireToken(withLogin: login).then {
+            let authenticatedClient = AuthenticatedHTTPClient(baseAddress: self.baseAddress, token: $0)
+            self.episodesService = EpisodeAPIService(authenticatedHTTPClient: authenticatedClient)
+            self.episodesService.fetchEpisodes(forShow: showIdentifier, page: page).catch { error in
+                guard let error = error as? HTTP.Error else { XCTFail("Incorrect error type"); return }
+                switch error {
+                case let .serverError(code, details):
+                    XCTAssertEqual(code, 404)
+                    print(String(describing: details))
+                default:
+                    XCTFail("Incorrect error type")
+                }
+                callbackExpectation.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
 }
